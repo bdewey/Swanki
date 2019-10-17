fb67f5cf3fee85e8f0cd33abfdfc09c9f7f4e7d7
Brian Dewey <bdewey@gmail.com>
2019-10-16 21:45:45 -0700

    Initial Commit

---
+++ Swanki/ContentView.swift
@@ -0,0 +1,15 @@
+// Copyright © 2019 Brian's Brain. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

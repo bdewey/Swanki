611f1b7e3cc4ae541c980c90174dad6a92342ae5
Brian Dewey <bdewey@gmail.com>
2019-12-19 05:42:24 -0800

    Use the simple Anki-compatible Mustache
    Doesn't support cloze or media yet; but now I have a place to add those.

--- SwankiTests/AnkiTemplateTests.swift
+++ SwankiTests/AnkiTemplateTests.swift
@@ -23,11 +23,21 @@ final class AnkiTemplateTests: XCTestCase {
 
   func testInvertedTruthySection() {
     let template = AnkiTemplate(template: "{{^boolean}}This should not be rendered.{{/boolean}}")
-    XCTAssertEqual(try? template.render(["boolean": true]), "")
+    XCTAssertEqual(try? template.render(["boolean": 312]), "")
   }
 
   func testInvertedFalseySection() {
     let template = AnkiTemplate(template: "{{^boolean}}This should be rendered.{{/boolean}}")
     XCTAssertEqual(try? template.render(), "This should be rendered.")
+  }
+
+  func testReplaceTag() {
+    let template = AnkiTemplate(template: "Hello {{name}}.")
+    XCTAssertEqual(try? template.render(["name": "Joe"]), "Hello Joe.")
+  }
+
+  func testAnkiAllowsSpaces() {
+    let template = AnkiTemplate(template: "Hello {{field 1}}.")
+    XCTAssertEqual(try? template.render(["field 1": "Jane"]), "Hello Jane.")
   }
 }
